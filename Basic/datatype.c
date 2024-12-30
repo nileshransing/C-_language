c88c4d8c18d0e08329e00abe1bcf02d903cb66eb
nileshransing <ransingnilesh7532@gmail.com>
2024-12-30 22:07:03 +0530

    first commit

---
+++ Basic/datatype.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+int main(){
+
+    printf("Hello Nilesh  \n");
+    
+
+    //  Primitive Data Type- Interger
+
+    int num=13;
+    printf("Number = %d \n",num);
+    
+
+   // float
+
+    float num1=13.45;
+    printf("N = %f \n",num1); 
+  
+
+// char
+    char n='N';
+    printf("Charater = %c \n",n);
+  
+
+// double
+
+    double d=45.45;
+    printf("marks = %lf \n",d);
+
+
+    // long - it take 8 digit Number
+    long bal=23466734;
+    printf("Balance = %ld \n",bal);
+
+    // long int
+    long int  longi=3434233434;
+    printf("Number = %ld \n",longi);
+
+    // short int
+    short int sint=343;
+    printf("Num = %hd \n",sint);
+
+    // unsigned int
+    unsigned int uint=23;
+    printf("Num = %ui \n",uint);
+
+    // Unsigned char
+  unsigned  char uc='!';
+    printf("Charater = %c \n",uc);
+
+    // long long int
+    long long int lli=52436876867;
+    printf("Big = %lld \n",lli);
+
+  return 0;
+}
