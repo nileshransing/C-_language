1bbf964ce002db26978e1f3afa39e260449807ba
nileshransing <ransingnilesh7532@gmail.com>
2025-01-02 12:02:45 +0530

    update

---
+++ C_Basic/Hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello KT");
+
+    printf("Hello Nilesh");
+    
+ return 0;
+}
--- Operations/Logical.c
+++ Operations/Logical.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * && And
  * || OR
@@ -23,3 +24,33 @@ int main(){
 
     return 0;
 }
+=======
+  
+  /*
+  Logical Operators  Not ! ,  And || ,   OR &&
+
+   */
+
+   #include <stdio.h>
+   int main(){
+
+
+   int a =56;
+   int b= 45;
+   int c=45;
+
+   // logical and &&
+    printf(" a>b && b>c: %d \n" , a>b && b>c);
+
+
+   // Logical  OR ||
+   printf(" b=c || a<c: %d \n", b=c || a<c);
+
+ // logical Not !
+  printf("  without Not  b=c || a>c : %d \n", (b=c || a>c));
+  printf("  with Not !( b=c || a>c ): %d \n", !(b=c || a>c));
+
+    return 0;
+
+   }
+>>>>>>> 46fb3bb (Intro_c)
