1ed096c854c402f621aea5a22fb97db3f40f108b
nileshransing <162991690+nileshransing@users.noreply.github.com>
2025-01-25 23:01:58 +0530

    Update stru_arr_stendentdata.c

--- Structure/stru_arr_stendentdata.c
+++ Structure/stru_arr_stendentdata.c
@@ -1,24 +1,35 @@
 #include<stdio.h>
 #include<string.h>
 
-struct studentdata{
-     int id;
+struct StudentData{
+
+    int id ;
     char name[20];
     double marks;
-}typedef studentdata;
 
+}typedef StudentData;
 
 int main(){
-   studentdata arr[2];
-    
-   for(int i=0;i<2;i++){
-    printf("Enter Student Id \n");
-    scanf("%d",arr[i].id);
-
-   }
-    
+    StudentData arr[2];
+    printf("%x\n",arr);
+  
+  // Taking array Input from User
     for(int i=0;i<2;i++){
-        printf("Id=%d\n",arr[i].id);
+    printf("Enter Student Id: ");
+    scanf("%d",&arr[i].id);
+
+    printf("Enter Student Name: ");
+    scanf("%s",&arr[i].name);
+
+    printf("Enter Student Marks: ");
+    scanf("%lf",&arr[i].marks);
+    }
+     // for printing Array
+     for(int i=0;i<2;i++){
+     printf("Id=%d\n",arr[i].id);
+     printf("Name=%s\n",arr[i].name);
+     printf("Marks=%lf\n",arr[i].marks);
+    
     }
     return 0;
 }
